9d719c3a4d6bb4bf709caa19368e4f69595f2c2d
Sahil Gupta <ffsg20067@gmail.com>
2025-08-16 14:19:04 +0530

    added llm api for giving scores based off the road images and text desciption

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,13 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, File, UploadFile, Form
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 from directions import get_route
+from typing import List
+import uuid
+import os
+import backend_llm
+import aiofiles
+import asyncio
 
 app = FastAPI()
 #Add api calling between front end and backend here
@@ -12,6 +19,41 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.post("/upload")
+async def upload(
+    images_bytes: List[UploadFile] = File(...),
+    text_descr: str = Form("")
+):
+    images = []
+
+    # images = []
+    if len(images_bytes) == 0 and text_descr == "":
+        return JSONResponse(content={"message": "no attached images and text description found!"}, status_code=400)
+    if len(images_bytes) != 0:
+        uuid_number = uuid.uuid4().hex
+        os.makedirs(f"uploads/upload_{uuid_number}", exist_ok=True)
+        ct = 0
+        for image in images_bytes:
+            f = image.file
+            file_path = f"upload_{uuid_number}/{ct}_{image.filename}"
+
+            contents = await image.read()
+            
+            async with aiofiles.open(f"uploads/{file_path}", "wb") as out_f:
+                await out_f.write(contents)
+
+            images.append({
+                "file_location": f"uploads/{file_path}",
+                "mime_type": image.content_type
+            })
+
+            ct += 1
+
+    scores = await asyncio.to_thread(backend_llm.get_scores, images, text_descr)
+    return JSONResponse(content=scores, status_code=200)
+
+
+    
 @app.get("/route")
 async def route(
     origin: str = Query(...),
